52f548320c35c41f3d0bf3417b5062c62f394320
Paulo Ricardo <132951001+PauloRicardoR@users.noreply.github.com>
2025-01-06 17:44:46 -0300

    Ajustando o time do scrollreveal - deixando mais rapido

--- reveal.js
+++ reveal.js
@@ -2,26 +2,26 @@ window.revelar = ScrollReveal({reset:true})
 
 //topo do site
 revelar.reveal('.efeito-txt', {
-    duration: 2000, 
-    distance: '90px',
+    duration: 1000, 
+    distance: '50px',
     origin: 'left'
 })
 
 revelar.reveal('.efeito-img', {
-    duration: 3000, 
-    distance: '90px', 
+    duration: 1000, 
+    distance: '50px', 
     delay: 500,
     origin: 'right'
 })
 
 //Sobre mim
 revelar.reveal('.titulo', {
-    duration: 2000, 
+    duration: 1000, 
     distance: '50px'
 })
 
 revelar.reveal('.efeito-p', {
-    duration: 2000, 
+    duration: 1000, 
     distance: '50px', 
     delay: 500
 })
@@ -29,26 +29,26 @@ revelar.reveal('.efeito-p', {
 //SKILLS
 
 revelar.reveal('.efeito-sk1', {
-    duration: 2000, 
+    duration: 1000, 
     distance: '50px', 
     delay: 500
 })
 
 revelar.reveal('.efeito-sk2', {
-    duration: 2000,
+    duration: 1000,
     distance: '50px',
     delay: 800
 })
 
 revelar.reveal('.efeito-sk3', {
-    duration: 2000,
+    duration: 1000,
     distance: '50px',
     delay: 1100
 })
 
 
 revelar.reveal('.efeito-sk4', {
-    duration: 2000,
+    duration: 1000,
     distance: '50px',
     delay: 1400
 })
