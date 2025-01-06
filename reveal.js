9c31668fef9dbf5a41add47a886d086a73ef7fe4
Paulo Ricardo <132951001+PauloRicardoR@users.noreply.github.com>
2025-01-06 15:27:06 -0300

    Att-site-efeitos-reveal

---
+++ reveal.js
@@ -0,0 +1,53 @@
+window.revelar = ScrollReveal({reset:true})
+
+//topo do site
+revelar.reveal('.efeito-txt', {
+    duration: 2000, 
+    distance: '90px',
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-img', {
+    duration: 3000, 
+    distance: '90px', 
+    delay: 500,
+    origin: 'right'
+})
+
+//Sobre mim
+revelar.reveal('.titulo', {
+    duration: 2000, 
+    distance: '50px'
+})
+
+revelar.reveal('.efeito-p', {
+    duration: 2000, 
+    distance: '50px', 
+    delay: 500
+})
+
+//SKILLS
+
+revelar.reveal('.efeito-sk1', {
+    duration: 2000, 
+    distance: '50px', 
+    delay: 500
+})
+
+revelar.reveal('.efeito-sk2', {
+    duration: 2000,
+    distance: '50px',
+    delay: 800
+})
+
+revelar.reveal('.efeito-sk3', {
+    duration: 2000,
+    distance: '50px',
+    delay: 1100
+})
+
+revelar.reveal('.efeito-sk4', {
+    duration: 2000,
+    distance: '50px',
+    delay: 1400
+})
